e3dbd36489c7d0b61a4748b679c6951d93ccb2ff
Lara <lara.lara@escola.pr.gov.br>
2022-11-05 21:19:47 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function createGame(player1, hour, player2) {
+  return `
+    <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+
+      <strong>${hour}</strong>
+
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+    </li>
+  `
+}
+
+let delay = -0.3;
+function createCard(date, day, games) {
+  delay = delay + 0.3;
+  return `
+    <div class="card" style="animation-delay: ${delay}s">
+      <h2>${date} <span>${day}</span></h2>
+
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML = 
+  createCard( "24/11", "quinta",
+    createGame('brazil', '16:00', 'serbia') +
+    createGame('brazil', '16:00', 'serbia')
+  ) +
+
+  createCard( "24/11", "quinta",
+    createGame('brazil', '13:00', 'cameroon')
+  ) +
+
+  createCard( "24/11", "quinta",
+    createGame('brazil', '13:00', 'switzerland')
+  )
